dbd69207b280124f5e9b27d1b56a8befaf99804f
SDAS98 <deyakosta98@gmail.com>
2026-04-30 16:43:18 +0200

    Pruebas

--- modulos/config.py
+++ modulos/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # Para pruebas en casa usa 127.0.0.1, en clase la 147.96.80.224
-    SERVER_URL: str = "http://127.0.0.1:7719/" 
+    SERVER_URL: str = "http://172.16.82.22:7719/" 
     OLLAMA_URL: str = "http://localhost:11434/api/chat"
     DEFAULT_MODEL: str = "ministral-3:8B"
     
